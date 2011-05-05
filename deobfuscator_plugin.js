3fb596b81800b59082ee2e19f81157c17df10b13
Nic <nicholas@wildinvention.co.uk>
2011-05-05 22:11:14 +0100

    adding plugin

---
+++ deobfuscator_plugin.js
@@ -0,0 +1,14 @@
+(function($){
+  $.fn.deobfuscate = function() {
+    $(this).each(function(i, el) {
+      var email, subject;
+      email = el.innerHTML;
+      email = email.replace(/ at /gi, "@").replace(/ dot /gi, ".");
+      subject = el.getAttribute('data-subject');
+      subject = subject ? ("?subject=" + subject) : ""
+      email = '<a href="mailto:'+email+subject+'">'+email+'</a>';
+      el.innerHTML = email;
+    });
+    return this;
+  };
+})(jQuery);;
